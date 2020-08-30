01c53f0d7ea31512f176b954e15b22a129e879ea
stosikmartyna <stosik.martyna@gmail.com>
2020-08-30 13:59:10 +0200

    Updates

---
+++ precache-manifest.95df7f0170cb51b2cbbeb85b5003730b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30e5ae6f288343bce190d495fb8b428",
+    "url": "/help_earth/index.html"
+  },
+  {
+    "revision": "c5f3c9cb5a8d9ef4cd67",
+    "url": "/help_earth/static/css/main.7c423019.chunk.css"
+  },
+  {
+    "revision": "5663e0bed6aa1b604a88",
+    "url": "/help_earth/static/js/2.92cfb98d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/help_earth/static/js/2.92cfb98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f3c9cb5a8d9ef4cd67",
+    "url": "/help_earth/static/js/main.69b141ea.chunk.js"
+  },
+  {
+    "revision": "d70cc69dc38f08d72419",
+    "url": "/help_earth/static/js/runtime-main.a93b2a3a.js"
+  },
+  {
+    "revision": "817fb321a8297c2751ef343c68ef4ee7",
+    "url": "/help_earth/static/media/LibreFranklin.817fb321.ttf"
+  }
+]);
